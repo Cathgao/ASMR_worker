6e0dd5aa3668e6f62f60002b1ab111c70ac0f6c9
Cathgao <cath@stu.zsc.edu.cn>
2022-06-06 00:41:18 +0800

    完成子目录文件抓取，根目录依旧报错

--- asmr.py
+++ asmr.py
@@ -22,19 +22,23 @@ url = 'https://api.asmr.one/api/tracks/' + RJ_number +  '?token=' + token
 # 调用requests.get方法对url进行访问，和持久化存储数据
 audio_content = requests.get(url=url, headers=headers).content
 jdata = json.loads(audio_content)
-for base in jdata[0]["children"]:
-    print("url: " + base["mediaDownloadUrl"])
-    baseUrl = base["mediaDownloadUrl"]
-    raw_path = os.path.split(urlparse(baseUrl).path.split("/",5)[-1])#获取url中的目录结构
-    file_path = urllib.parse.unquote(raw_path[0])#转码
-    print("path: " + file_path)
-    if os.path.exists(file_path): #检查目录结构是否存在
-        asmr = requests.get(baseUrl, headers=headers).content
-        with open(file_path + "/" + base["title"], 'wb') as f:
-            f.write(asmr)
-	
-    else:
-        #创建文件夹
-	    os.makedirs(file_path)
+print(jdata)
+
+# for base in jdata["children"]:
+for i in range(len(jdata)):
+    for base in jdata[i]["children"]:
+        print("url: " + base["mediaDownloadUrl"])
+        baseUrl = base["mediaDownloadUrl"]
+        raw_path = os.path.split(urlparse(baseUrl).path.split("/",5)[-1])#获取url中的目录结构
+        file_path = urllib.parse.unquote(raw_path[0])#转码
+        print("path: " + file_path)
+        if os.path.exists(file_path): #检查目录结构是否存在
+            asmr = requests.get(baseUrl, headers=headers).content
+            with open(file_path + "/" + base["title"], 'wb') as f:
+                f.write(asmr)
+        
+        else:
+            #创建文件夹
+            os.makedirs(file_path)
 
 print('下完了') 
