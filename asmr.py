d379233c6c7c08453900011171293ac06856fa3a
Cathgao <cath@stu.zsc.edu.cn>
2022-06-09 20:51:40 +0800

    Update asmr.py
    1.使用递归查询目录结构，解决多级目录造成的崩溃

--- asmr.py
+++ asmr.py
@@ -14,33 +14,36 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 token = NULL
 RJ_number = NULL
 user_info = {
-    "name" : NULL,
-    "password" : NULL
+    "name": NULL,
+    "password": NULL
 }
 UserAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15'
 
 #######使用账户名密码登录######
+
+
 def login():
     global user_info
     #账户名密码表单
     if ((user_info["name"] == NULL) or (user_info["password"] == NULL)):
         user_info = {
-            "name" : input("输入账户名"),
-            "password" : input("输入密码")
+            "name": input("输入账户名"),
+            "password": input("输入密码")
         }
     #headers信息
     loggin_headers = {
-        "User-Agent" : UserAgent,
-        "Uontent-Type" : "application/json"
+        "User-Agent": UserAgent,
+        "Uontent-Type": "application/json"
     }
     #检查登录状态
     print("正在登录...")
     print("账户名： " + user_info["name"])
     print("密码： " + user_info["password"])
-    res = requests.post(url='https://api.asmr.one/api/auth/me',data=user_info,headers=loggin_headers)
-    if res.status_code != 200: 
+    res = requests.post(url='https://api.asmr.one/api/auth/me',
+                        data=user_info, headers=loggin_headers)
+    if res.status_code != 200:
         return -1
-    else:#登录成功返回200，记录token
+    else:  # 登录成功返回200，记录token
         token_raw = res.content
         global token
         token = json.loads(token_raw)["token"]
@@ -48,13 +51,15 @@ def login():
         return 0
 
 #######下载实现########
+
+
 def download(url: str, fname: str):
     # 用流stream的方式获取url的数据
     resp = requests.get(url, stream=True)
     # 拿到文件的长度，并把total初始化为0
     total = int(resp.headers.get('content-length', 0))
     if os.path.exists(fname):
-        if os.path.getsize(fname) == total: #文件存在且大小相符
+        if os.path.getsize(fname) == total:  # 文件存在且大小相符
             print(fname + " 已下载，跳过")
             return
     # 初始化tqdm，传入总数，文件名等数据，接着就是写入，更新等操作了
@@ -68,6 +73,22 @@ def download(url: str, fname: str):
         for data in resp.iter_content(chunk_size=1024):
             size = file.write(data)
             bar.update(size)
+
+######递归检查所有目录结构######
+
+
+def rDirCheck(fileNode):
+    if(fileNode["type"] == "folder") and ("children" in fileNode):
+        for child in fileNode["children"]:
+            rDirCheck(child)
+    else:
+        baseUrl = fileNode["mediaDownloadUrl"]
+        raw_path = os.path.split(
+            urlparse(baseUrl).path.split("/", 5)[-1])  # 获取url中的目录结构
+        file_path = urllib.parse.unquote(raw_path[0])  # 转码
+        if not os.path.exists(file_path):  # 检查目录结构是否存在
+            os.makedirs(file_path)
+        download(baseUrl, file_path + "/" + fileNode["title"])  # 下载文件
 
 
 ######查询作品号########
@@ -86,25 +107,26 @@ def check(RJnumber: str):
     else:
         return 1
 
+
 #######检查配置文件######
 if os.path.exists("user.json"):
-    with open("user.json","r") as f:
+    with open("user.json", "r") as f:
         user_info = json.load(f)
     print("读取到已有账号")
     while(login() == -1):
         print("账户名或密码错误！")
         user_info = {
-        "name" : NULL,
-        "password" : NULL
+            "name": NULL,
+            "password": NULL
         }
-else:   
+else:
     while(login() == -1):
         print("账户名或密码错误！")
         user_info = {
-        "name" : NULL,
-        "password" : NULL
+            "name": NULL,
+            "password": NULL
         }
-with open("user.json","w")as f:
+with open("user.json", "w")as f:
     f.write(json.dumps(user_info))
 
 
@@ -124,29 +146,7 @@ else:
     audio_content = requests.get(url=url, headers=headers).content
     jdata = json.loads(audio_content)
     for i in range(len(jdata)):
-        if "children" in jdata[i]:
-            for base in jdata[i]["children"]:
-                baseUrl = base["mediaDownloadUrl"]
-                raw_path = os.path.split(urlparse(baseUrl).path.split("/", 5)[-1])  # 获取url中的目录结构
-                file_path = urllib.parse.unquote(raw_path[0])  # 转码
-                if os.path.exists(file_path): #检查目录结构是否存在
-                    download(baseUrl,file_path + "/" + base["title"]) #下载文件
-                else:
-                    #创建文件夹
-                    os.makedirs(file_path)
-                    download(baseUrl,file_path + "/" + base["title"]) #下载文件
-        else:  # 处理没有children的根目录url
-            baseUrl = jdata[i]["mediaDownloadUrl"]
-            a = urlparse(baseUrl).path.split("/", 5)
-            raw_path = os.path.split(
-                urlparse(baseUrl).path.split("/", 5)[-1])  # 获取url中的目录结构
-            file_path = urllib.parse.unquote(raw_path[0])  # 转码
-            if os.path.exists(file_path):  # 检查目录结构是否存在
-                download(baseUrl,file_path + "/" + jdata[i]["title"]) #下载文件
-            else:
-                # 创建文件夹
-                os.makedirs(file_path)
-                download(baseUrl,file_path + "/" + jdata[i]["title"]) #下载文件
+        rDirCheck(jdata[i])
     print('下载完毕')
     print('等待修改根目录名')
 
@@ -157,6 +157,6 @@ else:
     work_data = json.loads(work_content)
     root_path = "RJ" + str(work_data["id"])
     if os.path.exists(root_path):
-        os.rename(root_path,root_path + " " + work_data["title"])
+        os.rename(root_path, root_path + " " + work_data["title"])
     print('RJ' + RJ_number + "下载完毕")
 os.system("pause")
